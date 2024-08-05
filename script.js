6739d9a5e1e3d55044d11ff69d8ab8ac7f9f1b35
webstyle707 <webstyle707@gmail.com>
2024-08-05 11:37:28 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,140 @@
+function url() {
+    fbq("track", "Purchase");
+    location.href = "https://chat.whatsapp.com/DEwTmeThw96BqGjowUxCfE";
+  }
+
+function  btnhover1(){
+  let arrowhover = document.getElementById('arrowhover').style;
+  arrowhover.transform = "translateX(10px)"; 
+  arrowhover.transition = "all 0.5s ease";
+}
+
+function  btnleavehover1(){
+  let arrowhover = document.getElementById('arrowhover').style;
+  arrowhover.transition = "all 0.5s ease";
+  arrowhover.transform = "translateX(0px)"; 
+}
+
+
+function  btnhover2(){
+  let arrowhover2 = document.getElementById('arrowhover2').style;
+  arrowhover2.transform = "translateX(10px)"; 
+  arrowhover2.transition = "all 0.5s ease";
+}
+
+function  btnleavehover2(){
+  let arrowhover2 = document.getElementById('arrowhover2').style;
+  arrowhover2.transition = "all 0.5s ease";
+  arrowhover2.transform = "translateX(0px)"; 
+}
+
+
+function  btnhover3(){
+  let arrowhover3 = document.getElementById('arrowhover3').style;
+  let text1 = document.getElementById('text1').style;
+  text1.color = "#7eb8a6"
+  text1.transition = "all 0.5s ease";
+  arrowhover3.transform = "translateX(10px)"; 
+  arrowhover3.transition = "all 0.5s ease";
+}
+
+function  btnleavehover3(){
+  let arrowhover3 = document.getElementById('arrowhover3').style;
+  let text1 = document.getElementById('text1').style;
+  text1.color = "#2ba17e"
+  text1.transition = "all 0.5s ease";
+  arrowhover3.transition = "all 0.5s ease";
+  arrowhover3.transform = "translateX(0px)"; 
+}
+
+
+function  btnhover4(){
+  let text2 = document.getElementById('text2').style;
+  text2.color = "#7eb8a6"
+  text2.transition = "all 0.5s ease";
+  let arrowhover4 = document.getElementById('arrowhover4').style;
+  arrowhover4.transform = "translateX(10px)"; 
+  arrowhover4.transition = "all 0.5s ease";
+}
+
+function  btnleavehover4(){
+  let arrowhover4 = document.getElementById('arrowhover4').style;
+  let text2 = document.getElementById('text2').style;
+  text2.color = "#2ba17e"
+  text2.transition = "all 0.5s ease";
+  arrowhover4.transition = "all 0.5s ease";
+  arrowhover4.transform = "translateX(0px)"; 
+}
+
+
+function  btnhover5(){
+  let arrowhover5 = document.getElementById('arrowhover5').style;
+  let text3 = document.getElementById('text3').style;
+  text3.transition = "all 0.5s ease";
+  text3.color = "#7eb8a6"
+  arrowhover5.transform = "translateX(10px)"; 
+  arrowhover5.transition = "all 0.5s ease";
+}
+
+function  btnleavehover5(){
+  let arrowhover5 = document.getElementById('arrowhover5').style;
+  let text3 = document.getElementById('text3').style;
+  text3.color = "#2ba17e"
+  text3.transition = "all 0.5s ease";
+  arrowhover5.transition = "all 0.5s ease";
+  arrowhover5.transform = "translateX(0px)"; 
+}
+
+function  btnhover6(){
+  let arrowhover6 = document.getElementById('arrowhover6').style;
+  let text4 = document.getElementById('text4').style;
+  text4.transition = "all 0.5s ease";
+  text4.color = "#7eb8a6"
+  arrowhover6.transform = "translateX(10px)"; 
+  arrowhover6.transition = "all 0.6s ease";
+}
+
+function  btnleavehover6(){
+  let arrowhover6 = document.getElementById('arrowhover6').style;
+  let text4 = document.getElementById('text4').style;
+  text4.color = "#2ba17e"
+  text4.transition = "all 0.5s ease";
+  arrowhover6.transition = "all 0.6s ease";
+  arrowhover6.transform = "translateX(0px)"; 
+}
+
+function  btnhover7(){
+  let arrowhover7 = document.getElementById('arrowhover7').style;
+  let text5 = document.getElementById('text5').style;
+  text5.transition = "all 0.5s ease";
+  text5.color = "#7eb8a6"
+  arrowhover7.transform = "translateX(10px)"; 
+  arrowhover7.transition = "all 0.6s ease";
+}
+
+function  btnleavehover7(){
+  let arrowhover7 = document.getElementById('arrowhover7').style;
+  let text5 = document.getElementById('text5').style;
+  text5.color = "#2ba17e"
+  text5.transition = "all 0.5s ease";
+  arrowhover7.transition = "all 0.6s ease";
+  arrowhover7.transform = "translateX(0px)"; 
+}
+
+function  btnhover8(){
+  let arrowhover8 = document.getElementById('arrowhover8').style;
+  let text6 = document.getElementById('text6').style;
+  text6.color = "#7eb8a6"
+  text6.transition = "all 0.5s ease";
+  arrowhover8.transform = "translateX(10px)"; 
+  arrowhover8.transition = "all 0.6s ease";
+}
+
+function  btnleavehover8(){
+  let arrowhover8 = document.getElementById('arrowhover8').style;
+  let text6 = document.getElementById('text6').style;
+  text6.color = "#2ba17e"
+  text6.transition = "all 0.5s ease";
+  arrowhover8.transition = "all 0.6s ease";
+  arrowhover8.transform = "translateX(0px)"; 
+}
